0b15d408d26030a6bf91cab7d97999df5e83e654
Kyle Liwanag <ferninokyleliwanag@gmail.com>
2018-02-13 10:49:11 -0500

    Figure out arraylist on brick

--- src/Brick.java
+++ src/Brick.java
@@ -5,28 +5,28 @@ import java.util.List;
 public class Brick {
     private int brickLife = 3;
     private int x, y;
-    List<Brick> bricks = new ArrayList<Brick>();;
-    private final int WIDTH = 50, HEIGTH = 15;
+    private final int WIDTH = 50, HEIGHT = 15;
 
+    //initial position of each brick
     public Brick(){
         x = 0;
         y = 0;
+
     }
+
 
     //set position relative to middle of the brick
     public void setPosition(int x, int y){
         this.x = x - WIDTH/2;
-        this.y = y - HEIGTH/2;
+        this.y = y - HEIGHT/2;
     }
 
     public void paint(Graphics g){
-        g.fillRect(x, y, WIDTH, HEIGTH);
+        g.fillRect(x, y, WIDTH, HEIGHT);
     }
 
-    //add bricks to bricksArrayList
-    public void addBrick(Brick brick){
-        for(int i = 0; i < 4; i++)
-        bricks.add(0, brick);
-    }
+
+
+
 
 }
